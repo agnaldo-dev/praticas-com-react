c4e61156c92ca0cf50ed07a3020589802ef24cc7
Agnaldo dev <agnaldo.correa2012@gmail.com>
2021-10-15 11:40:33 -0300

    Componeten multiplos

--- pratica-react/src/index.js
+++ pratica-react/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM  from 'react-dom'
-import ComponenteFuncional from './components/ComponenteFuncional';
-import ComponenteFuncionalComParams from './components/ComponenteFuncionalComParams'
+//import ComponenteFuncional from './components/ComponenteFuncional';
+//import ComponenteFuncionalComParams from './components/ComponenteFuncionalComParams'
+import Multi,{ BoaNoite } from './components/Multi'
+
 //ReactDOM.render(<ComponenteFuncional/>,document.getElementById('root'))
 //passando paramentros para o componente
 ReactDOM.render(<ComponenteFuncionalComParams nome="Componente com parametro"/>,document.getElementById('root'))
+//usando multiplos componentes
+ReactDOM.render(<div>
+        <Multi.BoaTarde nome="Ana"/>
+        <BoaNoite nome ="Maria"/>
+        </div>,
+        document.getElementById('root'))
